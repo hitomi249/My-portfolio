25ab5b4efd7e3427668dd841cfa31f77f40b3f3b
hitomi249 <coloful.i.o.i@gmai.com>
2024-05-24 10:12:50 +0900

    js関連を追加

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+"use static";
+
+/*GSAP*/
+gsap.config({
+  nullTargetWarn: false,
+});
+
+/*ハンバーガーメニュー*/
+const hamburger = document.querySelector(".js_hamburger");
+const navigation = document.querySelector(".js_navigation");
+const body = document.querySelector(".js_body");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("is-active");
+  navigation.classList.toggle("is-active");
+  body.classList.toggle("is-active");
+});
+
+const navLinks = document.querySelectorAll(".l_header_nav_link");
+console.log(navLinks);
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    hamburger.classList.remove("is-active");
+    navigation.classList.remove("is-active");
+    body.classList.remove("is-active");
+  });
+});
