3d3125aa732534a10bda51e9f144e2df8002fd95
hitomi249 <coloful.i.o.i@gmai.com>
2024-05-27 21:40:43 +0900

    フォルダ名の変更

---
+++ docs/js/main.js
@@ -0,0 +1,105 @@
+"use static";
+
+/*GSAP*/
+gsap.config({
+  nullTargetWarn: false,
+});
+
+gsap
+  .timeline()
+  .from(".js_kv_ttl", {
+    duration: 1.2,
+    autoAlpha: 0,
+    y: 40,
+  })
+  .from(".js_header_ttl", {
+    duration: 1.2,
+    y: -70,
+  })
+  .from(".js_navigation, .js_hamburger", {
+    duration: 0.6,
+    x: 60,
+  });
+
+
+  gsap.from(".about_strength_desc::after", {
+    x: 100, // 100px右に移動
+    scrollTrigger: {
+      trigger: ".box", // アニメーション開始のトリガー要素
+      start: "top center", // アニメーション開始位置
+    },
+  });
+
+
+/*ハンバーガーメニュー*/
+const hamburger = document.querySelector(".js_hamburger");
+const navigation = document.querySelector(".js_navigation");
+const body = document.querySelector(".js_body");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("is-active");
+  navigation.classList.toggle("is-active");
+  body.classList.toggle("is-active");
+});
+
+const navLinks = document.querySelectorAll(".l_header_nav_link");
+console.log(navLinks);
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    hamburger.classList.remove("is-active");
+    navigation.classList.remove("is-active");
+    body.classList.remove("is-active");
+  });
+});
+
+/*headerのスクロールで表示/非表示切り替え*/
+const header = document.querySelector(".js_header");
+let lastScrollTop = 0;
+let ticking = false;
+
+function updateHeaderVisibility() {
+  const currentScrollTop = window.scrollY;
+
+  if (currentScrollTop < lastScrollTop || currentScrollTop <= 0) {
+    header.classList.remove("hidden");
+  } else {
+    header.classList.add("hidden");
+  }
+
+  lastScrollTop = currentScrollTop;
+  ticking = false;
+}
+
+window.addEventListener("scroll", () => {
+  if (!ticking) {
+    window.requestAnimationFrame(updateHeaderVisibility);
+    ticking = true;
+  }
+});
+
+/*背面固定*/
+// let isMenuOpen = false;
+// let scrollTopBeforeMenuOpen = 0;
+
+// // メニューボタンがクリックされたときの処理
+// hamburger.addEventListener("click", () => {
+//   if (!isMenuOpen) {
+//     // メニューを開くとき
+//     scrollTopBeforeMenuOpen = window.scrollY; // メニューを開く前のスクロール位置を記録
+//     body.style.overflow = "hidden"; // 背面スクロールを無効化
+//     body.style.position = "fixed"; // 背面を固定
+//     body.style.top = `-${scrollTopBeforeMenuOpen}px`; // スクロール位置を保持するために、bodyを上にオフセット
+//     header.classList.add("hidden"); // ヘッダーを非表示にする
+//     isMenuOpen = true;
+//   } else {
+//     // メニューを閉じるとき
+//     body.style.overflow = ""; // 背面スクロールを有効化
+//     body.style.position = ""; // 背面の固定を解除
+//     body.style.top = ""; // 上方オフセットを削除
+//     window.scrollTo(0, scrollTopBeforeMenuOpen); // メニューを開く前のスクロール位置に戻す
+//     header.classList.remove("hidden"); // ヘッダーを再表示する
+//     isMenuOpen = false;
+//   }
+// });
+
+// メニュー項目がクリックされたときにもメニューを閉じる処理を追加することが一般的です。
